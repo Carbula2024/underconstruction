0f480e24dcc63eed105d43f0e655cac218c69325
Carbula2024 <daniel@carbula.com>
2024-12-24 12:36:59 -0400

    features

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+export default {
   reactStrictMode: true,
+  output: 'export', // Exportación estática
+  distDir: 'build', // Configura el directorio de salida
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      config.resolve.fallback = { fs: false }; // Soluciona problemas con el módulo fs en el cliente
+    }
+    return config;
+  },
 };
-
-export default nextConfig;
